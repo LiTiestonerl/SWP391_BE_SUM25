5aefb8e8e675fb63fe06dd59e3636c774b6439c0
minhtien <cunconcut394@gmail.com>
2025-07-14 15:37:25 +0700

    feat: update SmokingStatusRequest validation constraints

--- src/main/java/com/example/smoking_cessation_platform/dto/smokingstatus/SmokingStatusRequest.java
+++ src/main/java/com/example/smoking_cessation_platform/dto/smokingstatus/SmokingStatusRequest.java
@@ -2,6 +2,7 @@ package com.example.smoking_cessation_platform.dto.smokingstatus;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
