398094efd6737a5c07c3fe9c4fcb217d3a47759f
24HGB <hoanggiabao9365@gmail.com>
2025-07-17 09:15:16 +0700

    add controller for admin/dashboard

---
+++ src/main/java/com/example/smoking_cessation_platform/controller/AdminStatsController.java
@@ -0,0 +1,10 @@
+package com.example.smoking_cessation_platform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/dashboard")
+public class AdminStatsController {
+
+}
