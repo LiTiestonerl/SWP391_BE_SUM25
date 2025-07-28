2e7c6d747ef4f83181b9ae42eee666debfeb9152
24HGB <hoanggiabao9365@gmail.com>
2025-07-28 11:41:27 +0700

    PreAuthorize for UserProfileController

--- src/main/java/com/example/smoking_cessation_platform/controller/UserProfileController.java
+++ src/main/java/com/example/smoking_cessation_platform/controller/UserProfileController.java
@@ -3,18 +3,22 @@ package com.example.smoking_cessation_platform.controller;
     import com.example.smoking_cessation_platform.dto.user.UserProfileRequest;
     import com.example.smoking_cessation_platform.dto.user.UserProfileResponse;
     import com.example.smoking_cessation_platform.service.UserService;
+    import io.swagger.v3.oas.annotations.security.SecurityRequirement;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.http.ResponseEntity;
+    import org.springframework.security.access.prepost.PreAuthorize;
     import org.springframework.web.bind.annotation.*;
 
     @RestController
     @RequestMapping("/api/user")
+    @SecurityRequirement(name = "api")
     public class UserProfileController {
 
         @Autowired
         private UserService userService;
 
         @PutMapping("/profile")
+        @PreAuthorize("hasRole('USER')")
         public ResponseEntity<UserProfileResponse> updateUserProfile(@RequestBody UserProfileRequest userProfileRequest) {
             try {
                 UserProfileResponse response = userService.updateUserProfile(userProfileRequest);
@@ -25,6 +29,7 @@ package com.example.smoking_cessation_platform.controller;
         }
 
         @GetMapping("/profile/{userId}")
+        @PreAuthorize("hasRole('ADMIN') or #userId == authentication.principal.id")
         public ResponseEntity<UserProfileResponse> getUserProfile(@PathVariable Long userId) {
             return userService.getUserProfile(userId)
                     .map(ResponseEntity::ok)
